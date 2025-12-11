cc0f05243ffa94b02957f5e17062c2e12ee5481d
Tom Kennedy <tom.kennedy@broadcom.com>
2025-12-11 17:08:01 -0500

    add caps word to nav layer
    Signed-off-by: Tom Kennedy <tom.kennedy@broadcom.com>

--- keyboards/boardsource/unicorne/keymaps/tomkennedy513/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/tomkennedy513/keymap.c
@@ -45,14 +45,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ┌───┬───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┬───┐
      * │   │   │   │   │   │   │   │HOM│PGD│PGU│END│DEL│   │
      * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
-     * │   │SFT│CTL│ALT│GUI│   │   │ ← │ ↓ │ ↑ │ → │BSP│   │
+     * │   │SFT│CTL│ALT│GUI│cw │   │ ← │ ↓ │ ↑ │ → │BSP│   │
      * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
      * │   │   │   │   │   │   │   │PTB│TAB│ESC│NTB│ENT│   │
      * └───┴───┴───┴───┴───┴───┘   └───┴───┴───┴───┴───┴───┘
      */
     [_NAV] = LAYOUT_split_3x6_3(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    RCS(KC_TAB), KC_PGDN, KC_PGUP, C(KC_TAB), KC_DEL,  XXXXXXX,
-        XXXXXXX, OS_SHFT, OS_CTRL, OS_ALT,  OS_GUI,  XXXXXXX,    KC_LEFT,     KC_DOWN, KC_UP,   KC_RGHT,   KC_BSPC, XXXXXXX,
+        XXXXXXX, OS_SHFT, OS_CTRL, OS_ALT,  OS_GUI,  CW_TOGG,    KC_LEFT,     KC_DOWN, KC_UP,   KC_RGHT,   KC_BSPC, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_HOME,     KC_TAB,  KC_ESC,  KC_END ,   KC_ENT,  XXXXXXX,
                                    XXXXXXX, _______, _______,    _______, _______, XXXXXXX
     ),
--- users/tomkennedy513/rules.mk
+++ users/tomkennedy513/rules.mk
@@ -2,3 +2,4 @@ SRC += tomkennedy513.c
 
 # Enable features
 REPEAT_KEY_ENABLE = yes
+CAPS_WORD_ENABLE = yes
