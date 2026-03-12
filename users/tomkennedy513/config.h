fcb121ba00e1cbfc208ef71c30500a122f70cc19
Tom Kennedy <kennedy513@gmail.com>
2026-03-12 12:05:02 -0400

    Change layer limeouts
    Signed-off-by: Tom Kennedy <kennedy513@gmail.com>

--- users/tomkennedy513/config.h
+++ users/tomkennedy513/config.h
@@ -2,8 +2,8 @@
 
 // Enable one-shot mod timeout
 // This applies to OSM(MOD_LSFT), OSM(MOD_LCTL), etc.
-#define ONESHOT_TIMEOUT 3000  // 3 seconds (3000ms)
+#define ONESHOT_TIMEOUT 1000 // 1 second (1000ms)
 
 // Optional: Configure one-shot layer timeout separately
 // (Used by custom implementation in tomkennedy513.c)
-#define ONESHOT_LAYER_TIMEOUT 3000  // 3 seconds (3000ms)
+#define ONESHOT_LAYER_TIMEOUT 300 // .3 seconds (300ms)
